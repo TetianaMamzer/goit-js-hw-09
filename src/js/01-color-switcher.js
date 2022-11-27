aad15b8748d745c95077797f1641eefa447648b4
TetianaMamzer <tetianamamzer@gmail.com>
2022-11-27 12:24:19 +0200

    +

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,6 +7,7 @@ const refs = {
 
 let timerId = null;
 
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
@@ -15,9 +16,12 @@ refs.btStart.addEventListener('click', () => {
   timerId = setInterval(() => {
  refs.body.style.backgroundColor = getRandomHexColor();
  refs.btStart.setAttribute('disabled', true);
+ refs.btStop.removeAttribute('disabled');
 }, 1000)
  });
 
 refs.btStop.addEventListener('click', () => {
   clearInterval(timerId);
+  refs.btStart.removeAttribute('disabled');
+  refs.btStop.setAttribute('disabled', true);
 })
